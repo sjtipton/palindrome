50fd7da35e2e7c1074c71bcf7f9e3a9273bae709
Jeff Self <jeff.self@gmail.com>
2012-09-20 11:31:46 -0400

    Added student model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120920142507) do
+ActiveRecord::Schema.define(:version => 20120920150342) do
 
   create_table "courses", :force => true do |t|
     t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "enrollments", :force => true do |t|
+    t.integer  "course_id"
+    t.integer  "student_id"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
@@ -39,6 +46,14 @@ ActiveRecord::Schema.define(:version => 20120920142507) do
     t.string   "last_name"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+  end
+
+  create_table "students", :force => true do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.integer  "institution_id"
+    t.datetime "created_at",     :null => false
+    t.datetime "updated_at",     :null => false
   end
 
 end
