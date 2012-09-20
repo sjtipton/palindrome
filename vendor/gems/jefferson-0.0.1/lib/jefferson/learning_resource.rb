646484278b31286d54771f47a5db22f1b0be7f4c
Steve Tipton <steve.tipton@employmentguide.com>
2012-09-20 12:46:41 -0400

    Unpack modified api_query

--- vendor/gems/jefferson-0.0.1/lib/jefferson/learning_resource.rb
+++ vendor/gems/jefferson-0.0.1/lib/jefferson/learning_resource.rb
@@ -33,24 +33,25 @@ class Jefferson::LearningResource
   end
 
   def self.api_query(opts={})
-    opts[:q] = opts[:any_tags] if opts[:any_tags].present?
+    opts[:any_tags] = opts[:any_tags] if opts[:any_tags].present?
     opts[:from] = opts[:from] if opts[:from].present?
     opts[:until] = opts[:until] if opts[:until].present?
     opts[:identity] = opts[:identity] if opts[:identity].present?
     opts[:ids_only] = opts[:ids_only] if opts[:ids_only].present?
     opts[:resumption_token] = opts[:resumption_token] if opts[:resumption_token].present?
-    options = opts.select { |k,v| %w(q from until identity ids_only resumption_token).include?(k.to_s) && v.present? }
+    options = opts.select { |k,v| %w(any_tags from until identity ids_only resumption_token).include?(k.to_s) && v.present? }
     options.symbolize_keys!
     query_params = options.to_param
-    request = Typhoeus::Request.new("https://http://node01.public.learningregistry.net/slice?#{query_params}",
+    request = Typhoeus::Request.new(Jefferson::Config.base_url +
+                                    "/slice?#{query_params}",
                                     { method: :get,
-                                     timeout: 10 }.merge(Jefferson::Config.headers))
+                                     timeout: Jefferson::Config.timeout }.merge(Jefferson::Config.headers))
 
     request.on_complete do |response|
       if response.success?
         learning_resources = []
         parsed = Yajl::Parser.parse(response.body, symbolize_keys: true)
-        parsed[:learning_resources].each do |result|
+        parsed[:documents].each do |result|
           learning_resources << new(id: result[:doc_ID])
         end
         yield learning_resources
@@ -60,7 +61,7 @@ class Jefferson::LearningResource
         raise ActiveResource::UnauthorizedAccess.new(response)
       elsif (500..599).cover? response.code
         raise ActiveResource::ServerError.new(response)
-      elsif response.timed_out? && response.request.retries <= 0
+      elsif response.timed_out?
         raise ActiveResource::TimeoutError.new(response.curl_error_message)
       else
         raise ActiveResource::ConnectionError.new(response)
