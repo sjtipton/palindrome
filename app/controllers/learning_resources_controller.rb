ac6913c9f9fc2351686970699bd2a668c33eb22f
Steve Tipton <steve.tipton@employmentguide.com>
2012-09-20 16:40:09 -0400

    Add learning_resources#index

--- app/controllers/learning_resources_controller.rb
+++ app/controllers/learning_resources_controller.rb
@@ -1,6 +1,13 @@
 class LearningResourcesController < ApplicationController
 
-	def index
-	end
+  def index
+    @search_query_params = { keywords: params[:keywords] }
+    @search_query = SearchQuery.new(@search_query_params)
+
+    if @search_query.valid?
+      Jefferson::LearningResource.api_query(any_tags: @search_query.keywords) { |lrs| @learning_resources = lrs }
+      Jefferson::Config.hydra.run
+    end
+  end
 
 end
