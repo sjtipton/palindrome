4afbaa5801b7689afac8c1a7b0b5565fe4659dbf
Steve Tipton <steve.tipton@employmentguide.com>
2012-09-20 08:30:46 -0400

    initial commit for new rails app: palindrome

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Palindrome::Application.config.secret_token = '6f781ed58c5be4b2459a5fa8555416eeed175e2c7305e6913c16bbc557df1eb502feb4fbb651e77af43affc51bfdbe60d13780359929e685e9705efe3694381d'
