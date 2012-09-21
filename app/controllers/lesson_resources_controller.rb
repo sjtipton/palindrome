d755e01d823348778dc692ae634ed2ffe5af66c1
Steve Tipton <steve.tipton@employmentguide.com>
2012-09-21 12:32:09 -0400

    Re-instantiate objects that show in re-rendered partial

--- app/controllers/lesson_resources_controller.rb
+++ app/controllers/lesson_resources_controller.rb
@@ -3,6 +3,12 @@ class LessonResourcesController < ApplicationController
 
   def create
   	@lesson_plan = LessonPlan.find(params[:lesson_plan_id])
+    @lesson_plan_resources = @lesson_plan.lesson_plan_resources
+    @similar_lesson_plans = @lesson_plan.similar_lesson_plans
+
+    tags = @lesson_plan.topic.gsub(", ", ",")
+    Jefferson::LearningResource.api_query(any_tags: tags) { |lrs| @learning_resources = lrs }
+    Jefferson::Config.hydra.run
   	resource_id = params[:lesson_plan_resource][:resource_id]
 
   	respond_to do |format|
