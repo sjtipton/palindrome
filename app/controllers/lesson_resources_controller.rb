699890eba8649f09ac63716383a232335c799f53
Steve Tipton <steve.tipton@employmentguide.com>
2012-09-21 12:19:28 -0400

    Add to Lesson Plan via ajax

--- app/controllers/lesson_resources_controller.rb
+++ app/controllers/lesson_resources_controller.rb
@@ -1,3 +1,15 @@
 class LessonResourcesController < ApplicationController
   before_filter :authenticate_instructor!
+
+  def create
+  	@lesson_plan = LessonPlan.find(params[:lesson_plan_id])
+  	resource_id = params[:lesson_plan_resource][:resource_id]
+
+  	respond_to do |format|
+	  	if @lesson_plan.lesson_plan_resources.create(resource_id: resource_id)
+        flash.now[:notice] = "Successfully saved Resource to your Lesson Plan"
+        format.js
+	  	end
+    end
+  end
 end
